98c89fe43d1a5b6c0069207d37b0b020f9c50cfa
PhilipHoltzman <phil.holtzman@gmail.com>
2016-03-09 11:43:44 -0600

    completed cipher

--- atbashCipher.py
+++ atbashCipher.py
@@ -1,4 +1,16 @@
 #! Python3 
 
 # Atbash Cipher from /r/dailyprogrammer
+# To encode or decode strings using the Atbash Cipher
 
+# Plain:  abcdefghijklmnopqrstuvwxyz
+
+message = 'Fear is the mindkiller'
+
+alephbet = 'abcdefghijklmnopqrstuvwxyz'
+
+cipher = alephbet[::-1]
+
+sub = dict(zip(alephbet,cipher))
+
+print(''.join( map( lambda x: sub[x] if x in sub else x ,message)))
